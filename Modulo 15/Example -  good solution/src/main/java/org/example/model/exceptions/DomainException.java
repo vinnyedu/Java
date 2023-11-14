b29db252970dc281fb9c0540c923d9d0bd145b92
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-11-14 19:32:33 -0300

    good solution

---
+++ Modulo 15/Example -  good solution/src/main/java/org/example/model/exceptions/DomainException.java
@@ -0,0 +1,13 @@
+package org.example.model.exceptions;
+
+import java.io.Serial;
+
+public class DomainException extends Exception{
+
+    @Serial
+    private static final long serialVersionUID = 2991346862769337110L;
+
+    public DomainException(String msg){
+        super(msg);
+    }
+}
