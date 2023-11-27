8cb1eaba24830a0174657e27fcf5993fa4ca6075
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-11-27 12:35:01 -0300

    example 01

---
+++ Modulo18/Example01 - without interface/src/main/java/org/example/application/Program.java
@@ -0,0 +1,45 @@
+package org.example.application;
+
+import org.example.entities.CarRental;
+import org.example.entities.Vehicle;
+import org.example.services.BrazilTaxService;
+import org.example.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
+
+        System.out.print("Entre com o preço por hora: ");
+        Double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        Double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerHour, pricePerDay, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+        System.out.println("FATURA");
+        System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+        sc.close();
+    }
+}
---
+++ Modulo18/Example01 - without interface/src/main/java/org/example/services/BrazilTaxService.java
@@ -0,0 +1,11 @@
+package org.example.services;
+
+public class BrazilTaxService {
+    public Double tax(double amount){
+        if (amount <=  100) {
+            return amount * 0.2;
+        } else {
+            return amount * 0.15;
+        }
+    }
+}
