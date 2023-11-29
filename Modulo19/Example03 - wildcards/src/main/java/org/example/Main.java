c52703d78a30ad386a559fd5ee128269c8dc9ab8
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-11-29 13:10:42 -0300

    example 03

---
+++ Modulo19/Example03 - wildcards/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
+
+        List<String> myStr = Arrays.asList("Maria", "Alex", "Bob");
+        printList(myStr);
+    }
+
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
+        }
+    }
+}
