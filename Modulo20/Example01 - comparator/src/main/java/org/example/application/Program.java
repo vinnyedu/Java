2b308ca94fa30ec7aeda6c7dc36d1ab1d41893a9
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-12-01 14:40:25 -0300

    example 01 - comparator with lambda

---
+++ Modulo20/Example01 - comparator/src/main/java/org/example/application/Program.java
@@ -0,0 +1,24 @@
+package org.example.application;
+
+import org.example.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 450.0));
+
+        System.out.println();
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
+}
