5ac8937cd88164e3bcc3dd4c1278c50b1041d8fb
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-12-04 13:22:19 -0300

    exercise 01

---
+++ Modulo20/Exercise01/src/main/java/org/example/application/Program.java
@@ -0,0 +1,49 @@
+package org.example.application;
+
+import org.example.entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String path = "/tmp/in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            List<Product> list = new ArrayList<>();
+            String line = br.readLine();
+
+            while (line != null) {
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = br.readLine();
+            }
+
+            double avg = list.stream()
+                    .map(p -> p.getPrice())
+                    .reduce(0.0, (x, y) -> x + y) / list.size();
+
+            System.out.println("Average price: " + String.format("%.2f", avg));
+
+            Comparator<String> comparator = (s1, s2) -> s1.toUpperCase().compareTo(s2.toUpperCase());
+
+            List<String> names = list.stream()
+                    .filter(p -> p.getPrice() < avg)
+                    .map(Product::getName)
+                    .sorted(comparator.reversed())
+                    .toList();
+
+            names.forEach(System.out::println);
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
