811ed8c1852ca3e676bfafdc5f18d43f8843d9fd
Vinicius Machado <vinicius.cm.edu@gmail.com>
2023-12-12 17:03:00 -0300

    retrieve data

--- Modulo21 - JDBC/jdbc2/src/main/java/org/example/db/DB.java
+++ Modulo21 - JDBC/jdbc2/src/main/java/org/example/db/DB.java
@@ -2,9 +2,7 @@ package org.example.db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -43,4 +41,23 @@ public class DB {
         }
     }
 
+    public static void closeStatement(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new DBException(e.getMessage());
+            }
+        }
+    }
 }
